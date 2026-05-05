0ae9ead34260055eda0c9cf666b020f41989a3ac
adityajha77 <adityakartikey1259@gmail.com>
2026-05-05 19:10:06 +0530

    chore: add node polyfills plugin and stabilize websocket

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,10 +12,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react()],
-  define: {
-    global: "window",
-  },
+  plugins: [
+    react(),
+    nodePolyfills(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
